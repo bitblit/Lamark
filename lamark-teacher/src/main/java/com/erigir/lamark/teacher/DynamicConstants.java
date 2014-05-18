6b96f72a031e644cb60b7df6a6693567509d0cab
Chris Weiss <bitblit@gmail.com>
2014-05-17 19:01:25 -0700

    Removing teacher

--- lamark-teacher/src/main/java/com/erigir/lamark/teacher/DynamicConstants.java
+++
@@ -1,9 +0,0 @@
-package com.erigir.lamark.teacher;
-
-public class DynamicConstants {
-    private DynamicConstants() {
-        super(); // prevent instantiation
-    }
-
-    public static final String CODE_KEY = "xxxXXX-CODE-XXXxxx";
-}
