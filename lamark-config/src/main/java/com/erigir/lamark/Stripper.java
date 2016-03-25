bbfba1f020b600bdc0194674b6dabbd405548f00
Chris Weiss <chris.weiss@timeinc.com>
2016-03-25 11:27:08 -0700

    Checkpoint

--- lamark-config/src/main/java/com/erigir/lamark/Stripper.java
+++ lamark-config/src/main/java/com/erigir/lamark/Stripper.java
@@ -3,6 +3,7 @@ package com.erigir.lamark;
 import java.util.function.Function;
 
 /**
+ * Given an individual object, strip it down to just the genome
  * Created by cweiss1271 on 3/24/16.
  */
 public class Stripper<T> implements Function<Individual<T>, T>
--- lamark-config/src/main/java/com/erigir/lamark/Wrapper.java
+++ lamark-config/src/main/java/com/erigir/lamark/Wrapper.java
@@ -3,6 +3,7 @@ package com.erigir.lamark;
 import java.util.function.Function;
 
 /**
+ * Given a genome, wrap it into an individual object
  * Created by cweiss1271 on 3/24/16.
  */
 public class Wrapper<T> implements Function<T, Individual<T>>
